885d18608baa404d441fe53360202cc9d10ebce8
Azest_Kingscrown <kimtaehyuk123@gmail.com>
2026-05-27 16:19:30 +0900

    Fix description formatting in help.js

--- command/help.js
+++ command/help.js
@@ -10,7 +10,7 @@ help.SendMessage = async function (context) {
         const exampleEmbed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle("치지직 알림 봇 사용법")
-            .setDescription("치지직 스트리머의 커뮤니티 공지와 방송 시작 알림을 1분마다 확인하여 전송해주는 디스코드 봇입니다.\n\n스트리머 등록 완료 시 일회성으로 스트리머의 마지막 커뮤니티(공지) 내용과 방송제목(방송중이 아닐 시 마지막 방송 제목)이 나옵니다.\n\n등록 이후 봇이 1분마다 정보를 갱신하며, 알림이 오기까지 최대 1분이 소요 될 수 있습니다.\n\n 가이드 : https://azestkingscrown.cloud/chzzk-bot-guide\n\n")
+            .setDescription("치지직 스트리머의 커뮤니티 공지와 방송 시작 알림을 1분마다 확인하여 전송해주는 디스코드 봇입니다.\n\n스트리머 등록 완료 시 일회성으로 스트리머의 마지막 커뮤니티(공지) 내용과 방송제목(방송중이 아닐 시 마지막 방송 제목)이 나옵니다.\n\n등록 이후 봇이 1분마다 정보를 갱신하며, 알림이 오기까지 최대 1분이 소요 될 수 있습니다.\n\n")
             .addFields(
                 {
                     name: '등록된 알림 리스트 확인',
